b7ac1484f460a5cf62373550f4c4d42e497ea0e1
Roger Ulate <roger.p.designe357@gmail.com>
2021-02-07 12:06:40 -0600

    Fin de clase 03

--- wk03/AppWk03/AppWk03/MainPage.xaml.cs
+++ wk03/AppWk03/AppWk03/MainPage.xaml.cs
@@ -13,6 +13,36 @@ namespace AppWk03
         public MainPage()
         {
             InitializeComponent();
+
+            dtp.Date = DateTime.Now;
+            dtp.MinimumDate = new DateTime(1990,1,1);
+            dtp.MaximumDate = new DateTime(2030,12,31);
+
+            editor.Completed += Editor_Completed;
+            editor.TextChanged += Editor_TextChanged;
+
         }
+
+        private void Editor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Editor_Completed(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            /* 
+             *  Esto recibe
+             *  DisplayAlert("string de el titulo del mensaje", "Texto de la alerta", "titulo de accion al hacerlick");
+            */
+            DisplayAlert("Alerta", "TEXT CHANGED", "Ok");
+        }
+        // En el code behind se programa todo lo que ocupemos que la herramienta haga o ejecute
+        // de acuerdo a la accion que se ejecute.
+
     }
 }
